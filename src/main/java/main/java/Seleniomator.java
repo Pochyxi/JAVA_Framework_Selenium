4de0ad74e922706fdbf430d1107d4aaa9d2acd1a
Pochyxi <adienerlopez@gmail.com>
2023-01-26 22:14:53 +0100

    updt

--- src/main/java/main/java/Seleniomator.java
+++ src/main/java/main/java/Seleniomator.java
@@ -1,15 +1,49 @@
 package main.java;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+
 public class Seleniomator {
-    private static final WebDriver driver = new ChromeDriver();
 
-    public static void openBrowser(String url) {
-        driver.get(url);
 
-        driver.close();
+    public static void runTests( List<String> arrayDiComandi ) {
+        // se non esistono istruzioni
+        if( arrayDiComandi.size() == 0 ) {
+            System.out.println("nessun comando da eseguire");
+        } else {
+            // apri chrome
+            WebDriver driver = new ChromeDriver();
+
+            // l'url da raggiungere
+            System.out.println( arrayDiComandi.get( 0 ).split( "->" )[ 1 ] );
+
+            // contatto url
+            driver.get( arrayDiComandi.get( 0 ).split( "->" )[ 1 ] );
+
+            // eseguo ciclo sui comandi
+            for( String comando : arrayDiComandi ) {
+
+                // i vari comandi da eseguire
+                switch (comando.split( "->" )[0]) {
+                    case "clicca" -> {
+                        // la stringa
+                        String cssSelector = comando.split( "->")[1];
+                        WebElement selected = driver.findElement(By.cssSelector( cssSelector ) );
+                        System.out.println(selected);
+                        selected.click();
+                        System.out.println(cssSelector);
+                    }
+                }
+            }
+
+            driver.close();
+        }
+
+
+//        driver.close();
     }
 }
--- src/main/java/main/java/XMLReader.java
+++ src/main/java/main/java/XMLReader.java
@@ -4,12 +4,15 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+
 import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class XMLReader {
     private static final String FILENAME = "src/filesXML/istruzioni.xml";
@@ -23,86 +26,74 @@ public class XMLReader {
 
             // optional, but recommended
             // process XML securely, avoid attacks like XML External Entities (XXE)
-            dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+            dbf.setFeature( XMLConstants.FEATURE_SECURE_PROCESSING, true );
 
             // parse XML file
             DocumentBuilder db = dbf.newDocumentBuilder();
 
-            Document doc = db.parse(new File(FILENAME));
+            Document doc = db.parse( new File( FILENAME ) );
 
             // optional, but recommended
-            // http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
             doc.getDocumentElement().normalize();
 
-            System.out.println("Elemento radice :" + doc.getDocumentElement().getNodeName());
-            System.out.println("------");
+            System.out.println( "Elemento radice :" + doc.getDocumentElement().getNodeName() );
+            System.out.println( "------" );
 
-            // get <comandi>
-            NodeList list = doc.getElementsByTagName("comandi");
+            // get <tests>
+            NodeList listTest = doc.getElementsByTagName( "test" );
 
             // itero gli elementi (nodi) all'interno della lista comandi
-            for (int temp = 0; temp < list.getLength(); temp++) {
+            for( int x = 0 ; x < listTest.getLength() ; x++ ) {
 
                 // il nodo che si trova all'interno di <Istruzioni>
-                Node node = list.item(temp);
+                Node test = listTest.item( x );
+                System.out.println( "Test '" + test.getAttributes().item( 0 ).getTextContent() + "'" );
 
                 // la lista di nodi che si trovano all'interno di <comandi>
-                NodeList listaDiComandi = node.getChildNodes();
-                System.out.println(listaDiComandi.getLength());
+                NodeList listaDiComandi = test.getChildNodes();
+                List<String> comandiDaEseguire = new ArrayList<>();
 
                 // itero la lista comandi
-                for (int i = 0; i < listaDiComandi.getLength(); i++) {
+                for( int i = 0 ; i < listaDiComandi.getLength() ; i++ ) {
 
                     // il singolo comando
-                    Node comando = listaDiComandi.item(i);
-                    if (comando.getNodeType() == Node.ELEMENT_NODE ) {
+                    Node comando = listaDiComandi.item( i );
 
-                        // se il comando ha un nodo figlio
-                        if (comando.hasChildNodes()) {
-                            Node istruzione = comando.getChildNodes().item(0);
-                            System.out.println(istruzione.getNodeName());
+                    //  Serve per trovare il nodo dentro al comando
+                    NodeList listaComando = comando.getChildNodes();
 
-                            if (istruzione.getNodeName() == "apri") {
-                                System.out.println("good");
+                    // prendo soli i nodi che mi interessano
+                    if( comando.getNodeType() == 1 ) {
+                        System.out.println( comando.getNodeName() );
+
+                        // itero il comando(solitamente avrà un elemento)
+                        for( int z = 0 ; z < listaComando.getLength() ; z++ ) {
+
+                            // il singolo comando (es. <clicca>tiscaTusca</clicca>)
+                            Node comandoDaEseguire = listaComando.item( z );
+
+                            // prendo solo il nodo che mi interessa
+                            if( comandoDaEseguire.getNodeType() == 1 ) {
+                                // la stringa che rappresenta il nome del tag(es. "clicca"
+                                String nomeComando = comandoDaEseguire.getNodeName();
+
+                                // la stringa che rappresenta il testo dentro al tag (es. "tiscaTusca")
+                                String infoComando = comandoDaEseguire.getTextContent();
+                                System.out.println( "trovato comando di nome '" + nomeComando + "' con testo '" + infoComando + "'" );
+
+                                // inserisco il comando nell'array
+                                comandiDaEseguire.add( nomeComando + "->" + infoComando );
                             }
                         }
                     }
-
-
-
-
-                    //System.out.println(comando.getTextContent());
                 }
-
-                /* if (node.getNodeType() == Node.ELEMENT_NODE) {
-
-                    Element element = (Element) node;
-
-                    // get comandi attribute
-                    String id = element.getAttribute("id");
-
-                    // get text
-                    String firstname = element.getElementsByTagName("firstname").item(0).getTextContent();
-                    String lastname = element.getElementsByTagName("lastname").item(0).getTextContent();
-                    String nickname = element.getElementsByTagName("nickname").item(0).getTextContent();
-
-                    NodeList salaryNodeList = element.getElementsByTagName("salary");
-                    String salary = salaryNodeList.item(0).getTextContent();
-
-                    // get salary's attribute
-                    String currency = salaryNodeList.item(0).getAttributes().getNamedItem("currency").getTextContent();
-
-                    System.out.println("Current Element :" + node.getNodeName());
-                    System.out.println("Staff Id : " + id);
-                    System.out.println("First Name : " + firstname);
-                    System.out.println("Last Name : " + lastname);
-                    System.out.println("Nick Name : " + nickname);
-                    System.out.printf("Salary [Currency] : %,.2f [%s]%n%n", Float.parseFloat(salary), currency);
-
-                } */
+                System.out.println( comandiDaEseguire );
+                // infine posso far partire i test grazie alla lista di comandi da eseguire
+                // essendo un ciclo tanti più test ci saranno tanti più liste verranno create
+                // ed eseguite
+                Seleniomator.runTests( comandiDaEseguire );
             }
-
-        } catch (ParserConfigurationException | SAXException | IOException e) {
+        } catch( ParserConfigurationException | SAXException | IOException e ) {
             e.printStackTrace();
         }
 
